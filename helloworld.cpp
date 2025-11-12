4526c9300cb336a7fef9ef1be2a584c7ea48457a
xianren <limhong050505@gmail.com>
2025-11-12 23:09:30 +0800

    enter hello

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,11 +6,5 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "hello world";
 }
