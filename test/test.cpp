d5b81b9e6b4c65ff3b85e8ae28606fd007568f62
xianren <limhong050505@gmail.com>
2025-12-11 16:20:19 +0800

    Add Project Folder and Project File

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name ;
+
+    cout << "please enter:";
+    cin >> name ;
+
+    system("pause");
+}
